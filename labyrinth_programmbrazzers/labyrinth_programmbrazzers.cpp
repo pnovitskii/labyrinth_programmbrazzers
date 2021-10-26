17da483e7a92068a82c189f9d225ee6215698ce5
Quanbe <79723178+Quanbe921@users.noreply.github.com>
2021-10-26 12:04:51 +0300

    first commit

--- labyrinth_programmbrazzers/labyrinth_programmbrazzers.cpp
+++ labyrinth_programmbrazzers/labyrinth_programmbrazzers.cpp
@@ -2,10 +2,31 @@
 //
 
 #include <iostream>
+#include <SFML\Graphics.hpp>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
