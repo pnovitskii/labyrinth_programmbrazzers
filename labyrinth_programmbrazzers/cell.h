f1bea6a34923095da2819696ddc730a8dc4c4c78
NikitaKaizer <kaizer.nikita@lll.kpi.ua>
2021-11-29 20:06:42 +0200

    fix

---
+++ labyrinth_programmbrazzers/cell.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
+class Cell
+{
+
+private:
+	Texture texture;
+public:
+	Sprite sprite;
+	bool path = false;
+	bool wall = false;
+	Cell(){}
+	Cell(int x, int y)
+	{
+		
+		sprite.setPosition(x * 10, y * 10);
+		
+	}
+//public:
+	void init(int x, int y);
+};
